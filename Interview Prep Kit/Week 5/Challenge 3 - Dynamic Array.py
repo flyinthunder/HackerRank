1a33e57e322d694794232be04e8e8797a34b80cb
flyinthunder <atharvajaaa@gmail.com>
2024-01-09 11:36:45 -0500

    Problem 3,4 of week 5 prep kit.

---
+++ Interview Prep Kit/Week 5/Challenge 3 - Dynamic Array.py
@@ -0,0 +1,43 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'dynamicArray' function below.
+#
+# The function is expected to return an INTEGER_ARRAY.
+# The function accepts following parameters:
+#  1. INTEGER n
+#  2. 2D_INTEGER_ARRAY queries
+#
+
+def dynamicArray(n, queries):
+    arr = [[] for i in range(n)]
+    last_answer = 0
+    answers_arr = []
+    for i in queries:
+        query = i
+        index = (query[1] ^ last_answer) % n
+        if query[0] == 1:
+            arr[index].append(query[2])
+        elif query[0] == 2:
+            last_answer = arr[index][query[2] % len(arr[index])]
+            answers_arr.append(last_answer)
+    return answers_arr
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+    first_multiple_input = input().rstrip().split()
+    n = int(first_multiple_input[0])
+    q = int(first_multiple_input[1])
+    queries = []
+    for _ in range(q):
+        queries.append(list(map(int, input().rstrip().split())))
+    result = dynamicArray(n, queries)
+    fptr.write('\n'.join(map(str, result)))
+    fptr.write('\n')
+    fptr.close()
